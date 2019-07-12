c755f386229cfc70585a6c5266f77fe9800596e4
Manoel <manoel.lobo@gmail.com>
2019-07-12 09:35:27 -0300

    global styles

---
+++ src/styles/global.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+import bg from '../assets/images/bg.svg';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #193034 url(${bg}) no-repeat center top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button{
+    font-size: 14px;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin:0 auto;
+    padding: 0 20px 50px;
+  }
+
+  button{
+    cursor: pointer;
+  }
+`;
