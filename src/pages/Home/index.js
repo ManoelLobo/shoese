114e10845b611b1ab3eb323eef6c28fa17c4c441
Manoel <manoel.lobo@gmail.com>
2019-07-12 11:39:56 -0300

    Home page styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://dummyimage.com/300x300/fff/000000&text=This+is+a+shoe+picture"
+          alt="Sneakers"
+        />
+        <strong>Super Stepper</strong>
+        <span>$ 99.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f6f6fa" /> 6
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://dummyimage.com/300x300/fff/000000&text=This+is+a+shoe+picture"
+          alt="Sneakers"
+        />
+        <strong>Super Stepper</strong>
+        <span>$ 99.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f6f6fa" />6
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://dummyimage.com/300x300/fff/000000&text=This+is+a+shoe+picture"
+          alt="Sneakers"
+        />
+        <strong>Super Stepper</strong>
+        <span>$ 99.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f6f6fa" />6
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://dummyimage.com/300x300/fff/000000&text=This+is+a+shoe+picture"
+          alt="Sneakers"
+        />
+        <strong>Super Stepper</strong>
+        <span>$ 99.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f6f6fa" />6
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://dummyimage.com/300x300/fff/000000&text=This+is+a+shoe+picture"
+          alt="Sneakers"
+        />
+        <strong>Super Stepper</strong>
+        <span>$ 99.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f6f6fa" />6
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://dummyimage.com/300x300/fff/000000&text=This+is+a+shoe+picture"
+          alt="Sneakers"
+        />
+        <strong>Super Stepper</strong>
+        <span>$ 99.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#f6f6fa" />6
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const ProductList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  list-style: none;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    background: #f6f6fa;
+    border-radius: 4px;
+    padding: 20px;
+
+    img {
+      align-self: center;
+      max-width: 250px;
+    }
+
+    > strong {
+      font-size: 16px;
+      line-height: 20px;
+      color: #3e4d58;
+      margin-top: 5px;
+    }
+
+    > span {
+      font-size: 21px;
+      font-weight: bold;
+      margin: 5px 0 20px;
+    }
+
+    button {
+      display: flex;
+      align-items: center;
+      background: #045488;
+      color: #f6f6fa;
+      border: 0;
+      border-radius: 4px;
+      margin-top: auto;
+      text-transform: uppercase;
+      transition: background 0.4s;
+
+      &:hover {
+        background: ${darken(0.05, '#045488')};
+      }
+
+      div {
+        display: flex;
+        align-items: center;
+        padding: 12px;
+        background: rgba(0, 0, 0, 0.1);
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+
+      span {
+        flex: 1;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
+  }
+`;
