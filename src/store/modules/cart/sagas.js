23231cc9675edf3ca76f99a14cd48e724dcfa3f8
Manoel <manoel.lobo@gmail.com>
2019-07-14 17:08:58 -0300

    put add to cart logic to saga

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,13 +1,28 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 
 import api from '../../../services/api';
+import { formatPrice } from '../../../utils/format';
 
 function* addToCart({ productId }) {
-  const response = yield call(api.get, `/products/${productId}`);
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === productId)
+  );
 
-  yield put(addToCartSuccess(response.data));
+  if (productExists) {
+    yield put(updateAmount(productId, productExists.amount + 1));
+  } else {
+    const response = yield call(api.get, `/products/${productId}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      formattedPrice: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
